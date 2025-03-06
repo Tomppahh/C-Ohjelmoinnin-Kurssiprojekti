75dd63a295c908dc6f0fa79fa9f162f5b4b88421
TuomasLHeinonen <tuomas.l.heinonen@student.lut.fi>
2025-03-06 17:14:20 +0200

    Perus pohja luotu (Valikko, kysy tiedosto nimi)

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # CT60A2600-Tiimi03
 
 # Käytetty aika L08:
-# Tuomas: (1h Luento, 1h Miitti)
+# Tuomas: (1h Luento, 1h Miitti, 1h WSL toiminnallisuus)
 # Tommi (1h Miitti)
 # Eelis (1h Miitti)
--- main.c
+++ main.c
@@ -2,10 +2,57 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define LENGTH 30
+
+int menu (void) {
+    int iSelection = 0;
+    printf("1) Lue tiedosto\n");
+    printf("2) Tallenna lista etuperin\n");
+    printf("3) Tallenna lista takaperin\n");
+    printf("4) Tyhjennä lista\n");
+    printf("0) Lopeta");
+    printf("Anna valintasi: ");
+    scanf("%d", &iSelection);
+    getchar();
+    return(iSelection);
+}
+
+void filename(char *pFileName, char *pSentence) {
+    printf("%s", pSentence);
+    scanf("%s", pFileName);
+    getchar();
+    return;
+}
+
 
 int main (void) {
-    printf("Hello World!\n");
+    int iSelection;
+    char aReadName[LENGTH], aWriteName[LENGTH];
 
+    do {
+        iSelection = menu();
+
+        if(iSelection == 1) {
+            filename(aReadName, "Anna luettavan atiedoston nimi: ");
+
+        } else if (iSelection == 2) {
+            filename(aWriteName, "Anna kirjoitettavan tiedoston nimi: ");
+
+        } else if (iSelection == 3) {
+            filename(aWriteName, "Anna kirjoitettavan tiedoston nimi: ");
+
+        } else if (iSelection == 4) {
+
+        } else if (iSelection == 0) {
+            printf("Lopetetaan.\n");
+        } else {
+            printf("Yritä uudestaan.\n");
+        }
+
+        printf("\n");
+
+    } while(iSelection != 0);
+    printf("Kiitos ohjelman käytöstä.");
     return (0);
 }
 
