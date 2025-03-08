38d3f65cf38fe8a50e8f778b35375bdc47f6c7f9
TuomasLHeinonen <tuomas.l.heinonen@student.lut.fi>
2025-03-09 00:41:39 +0200

    5 -> 0

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 
 
 int menu (void) {
-    int iSelection = 5;
+    int iSelection = 0;
     printf("Valitse haluamasi toiminto:\n");
     printf("1) Lue tiedosto\n");
     printf("2) Tallenna lista etuperin\n");
