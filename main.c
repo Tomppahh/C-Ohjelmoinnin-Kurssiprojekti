62eb8a5f4588071c78bb9d48f2876f7c95a22994
TuomasLHeinonen <tuomas.l.heinonen@student.lut.fi>
2025-03-08 15:25:13 +0200

    addNode aliohjelma

--- main.c
+++ main.c
@@ -42,6 +42,32 @@ CUSTOMER *empty(CUSTOMER *pA) {
     return(pA);
 }
 
+NODE *addNode(NODE *pA, char *pName, int iCount) {
+    NODE *pNew = NULL, *ptr = NULL;
+
+    if ((pNew = (NODE*)malloc(sizeof(NODE))) == NULL) {
+        perror("Muistin varaus epäonnistui, lopetetaan");
+        exit(0);
+    }
+
+    strcpy(pNew->pName, pName);
+    pNew->iCount = iCount;
+    pNew->pNext = NULL;
+    pNew->pPrev = NULL;
+
+    if (pA == NULL) {
+        pA = pNew;
+    } else {
+        ptr = pA;
+        while (ptr->pNext != NULL) {
+            ptr = ptr->pNext;
+        }
+        ptr->pNext = pNew;
+    }
+
+    return(pA);
+}
+
 int main (void) {
     CUSTOMER *pStart = NULL, *pEnd = NULL;
     CUSTOMER *pNew = NULL, *ptr = NULL;
