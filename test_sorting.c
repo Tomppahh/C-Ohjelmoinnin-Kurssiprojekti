9dec56dc8f7965aa8c75b5bd5e6783099520bbe5
Tomppahh <tommi.paakkunainen@student.lut.fi>
2025-03-14 18:25:35 +0200

    added more tests

--- test_sorting.c
+++ test_sorting.c
@@ -9,7 +9,7 @@
 // function to print the linked list, takes in the created list and the title for printing.
 void printList(NODE *head, const char *title)
 {
-    printf("%s\n", title);
+    printf("\n%s\n", title);
     if (head == NULL)
     {
         printf("list is empty\n\n");
@@ -56,6 +56,8 @@ int main() {
     printList(list, "Test 1b: mergeSort(Sort Descending) function with empty list");
     runTest(&passed, &failed, list == NULL, "mergeSort with empty list");
 
+
+
     // Test 2: Add a single node to the list
     list = createNode(list, "SingleNode", 1);
     updatePreviousPointers(list); // update struct pointers
@@ -71,6 +73,34 @@ int main() {
     updatePreviousPointers(list); // update struct pointers again
     printList(list, "Test 2b: mergeSort(Sort Descending) function with single node list");
     runTest(&passed, &failed, list != NULL && strcmp(list->aName, "SingleNode") == 0 && list->iCount == 1, "mergeSort with single node list");
+
+    // Free the single node for the next test
+    list = empty(list);
+
+
+
+    // Test 3: Add multiple nodes to the list
+    list = createNode(list, "Banana", 5);
+    list = createNode(list, "Aatami", 3);
+    list = createNode(list, "Chevrolet", 8);
+    list = createNode(list, "Daavid", 2);
+    updatePreviousPointers(list); // update struct pointers
+
+    list = sortAscending(list);
+    updatePreviousPointers(list); // update struct pointers again
+    printList(list, "Test 3a: sortAscending function with multiple nodes");
+    runTest(&passed, &failed, list != NULL && strcmp(list->aName, "Daavid") == 0 && list->iCount == 2, "sortAscending with multiple nodes");
+
+    list = mergeSort(list);
+    updatePreviousPointers(list); // update struct pointers again
+    printList(list, "Test 3b: mergeSort(Sort Descending) function with multiple nodes");
+    runTest(&passed, &failed, list != NULL && strcmp(list->aName, "Chevrolet") == 0 && list->iCount == 8, "mergeSort with multiple nodes");
+
+    // More tests can be added here
+    
+    // ****************************
+    // Lets free all memory at the end
+    list = empty(list);
 
     // Print summary of passed and failed tests
     printf("\nSummary: %d tests passed, %d tests failed\n", passed, failed);
