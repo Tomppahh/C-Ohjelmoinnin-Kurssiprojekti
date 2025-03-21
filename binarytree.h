8ff8502319c65f8ab739e6e9c9f99ff470be9611
TuomasLHeinonen <tuomas.l.heinonen@student.lut.fi>
2025-03-21 18:22:15 +0200

    Lisätty askValue

--- binarytree.h
+++ binarytree.h
@@ -19,6 +19,7 @@ NODE_BT* createTreeNode(const char* name, int number);
 NODE_BT* insertNode(NODE_BT* root, const char* name, int number);
 NODE_BT* buildFromFile(const char* filename);
 void writeFileTree(const char* filename, NODE_BT* root);
+int askValue(void);
 void printTree(NODE_BT* root);
 void printTreeHelper(NODE_BT* root, int iSpace, int* iPrinted, int iMaxPrinted);
 
