b1f7d06854f083dc049b84acc659d60f78a3ecf9
Tomppahh <tommi.paakkunainen@student.lut.fi>
2025-03-30 00:40:12 +0200

    small updates to binarytreesearch

--- main.c
+++ main.c
@@ -134,8 +134,7 @@ int main (void) {
                     printf("Anna etsittävä nimi tai numero: ");
                     fgets(searchTerm, sizeof(searchTerm), stdin);
                     searchTerm[strcspn(searchTerm, "\n")] = 0; // get rid of newline
-                    // filename(aWriteName, "Anna kirjoitettavan tiedoston nimi: ");  (uncomment if want write to file, also uncomment fastestSearchToFile write stuff!)
-                    BinaryTreeSearch(pStartTree, searchTerm, aWriteName);
+                    BinaryTreeSearch(pStartTree, searchTerm);
                 } else if (iSubSelection == 6) {
                     printTree(pStartTree);
                 } else if (iSubSelection == 7) {
