04ed0c537ba9ebf315da1899520042eb0bafc88d
TuomasLHeinonen <tuomas.l.heinonen@student.lut.fi>
2025-03-30 18:42:44 +0300

    Korjaus

--- avl_tree.h
+++ avl_tree.h
@@ -6,7 +6,7 @@
 int height(NODE_BT *node);
 int max(int a, int b);
 NODE_BT *rotateRight(NODE_BT *y);
-NODE_BT *rotateLeft(NODE_BT *x)
+NODE_BT *rotateLeft(NODE_BT *x);
 int getBalance(NODE_BT *node);
 NODE_BT* createNode_AVL(const char* name, int number);
 NODE_BT* insertNode_AVL(NODE_BT* node, const char* name, int number);
