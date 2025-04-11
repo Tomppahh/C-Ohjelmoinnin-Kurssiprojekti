d7595c171427c940da906fd1a4a8324cc24387a4
TuomasLHeinonen <tuomas.l.heinonen@student.lut.fi>
2025-04-11 17:27:10 +0300

    Testaus

--- common.h
+++ common.h
@@ -1,6 +1,10 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#include "linkedlist.h"
+#include "binarytree.h"
+#include "graph.h"
+
 void filename(char *pFileName, char *pSentence);
 NODE_BT* createTreeNode(const char* name, int number);
 
